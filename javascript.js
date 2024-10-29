1315f9bc646192ef9aada7469469d1de2267ba2b
Shouma Kawamura <ee217088@meiji.ac.jp>
2024-10-29 20:28:04 +0900

    js yomikomi

---
+++ javascript.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded",function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.tooltip(tooltipTriggerEl)
+  })
+});
